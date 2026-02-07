bf5383c5f372d5afcb83f49423e6d923e92dc2ad
sylvainyang <sylvainyang@tencent.com>
2026-02-07 20:02:52 +0800

    接入千问

--- pkg/aliyun/provider.go
+++
@@ -1,9 +0,0 @@
-package aliyun
-
-import "github.com/mylxsw/glacier/infra"
-
-type Provider struct{}
-
-func (Provider) Register(binder infra.Binder) {
-	binder.MustSingleton(New)
-}
--- pkg/sms/provider.go
+++
@@ -1,9 +0,0 @@
-package sms
-
-import "github.com/mylxsw/glacier/infra"
-
-type Provider struct{}
-
-func (Provider) Register(binder infra.Binder) {
-	binder.MustSingleton(New)
-}
